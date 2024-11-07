b65e1f2acbb66f8f3696232ba65381479c3a0835
José Maike <jose.maike@senado.leg.br>
2024-11-07 10:30:30 -0300

    first commit

---
+++ src/main/java/br/com/maike/order_managment_ssm/OrderManagmentSsmApplication.java
@@ -0,0 +1,13 @@
+package br.com.maike.order_managment_ssm;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OrderManagmentSsmApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OrderManagmentSsmApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/maike/order_managment_ssm/OrderManagmentSsmApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.maike.order_managment_ssm;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OrderManagmentSsmApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
